00f887ffbaabd74e79084fbb795f2e3be4ef2ee7
Juhye0k <kyoung1678@naver.com>
2025-11-02 16:35:51 +0900

    중간 2차

--- adapters/https_tls.py
+++ adapters/https_tls.py
@@ -20,7 +20,9 @@ BENCH_HDR_VERSION = 1
 # TLS 1.3 Cipher Suite names
 TLS13_CIPHERS = {
     "aesgcm": "TLS_AES_128_GCM_SHA256",
+    "aes256gcm": "TLS_AES_256_GCM_SHA384",
     "chacha20": "TLS_CHACHA20_POLY1305_SHA256",
+    "aesccm": "TLS_AES_128_CCM_SHA256",
     "aesccm8": "TLS_AES_128_CCM_8_SHA256",
 }
 
